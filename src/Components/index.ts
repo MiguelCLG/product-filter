cefee36c9de6d0f2add3fb7c18325f28faf4c078
Miguel Gonçalves <miguelclg@hotmail.com>
2025-02-02 17:44:30 +0000

    Updates

--- src/Components/index.ts
+++
@@ -1,7 +0,0 @@
-import Operators from "./Operators/Operators";
-import PropertyNames from "./PropertyNames/PropertyNames";
-import PropertyValues from "./PropertyValues/PropertyValues";
-import ClearButton from "./ClearButton/ClearButton";
-import FilteredList from "./FilteredList/FilteredList";
-
-export { Operators, PropertyNames, PropertyValues, ClearButton, FilteredList };
